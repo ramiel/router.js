4c36cde2c85b7a4ba64fcb6bc9f3bee09a6ba175
Fabrizio Ruggeri <fabrizio.ruggeri@gmail.com>
2015-06-04 23:32:56 +0200

    Fixed non-sense tests

--- tests/router.js
+++ tests/router.js
@@ -19,7 +19,7 @@ console.warn=null;
 
 describe("Router suite.", function() {
 	it("Check that Router exists and is a Function", function() {
-  		expect(Router).to.be.ok();
+  		expect(Router).to.be.a(Function);
 	});
 
 	var router;
@@ -27,86 +27,77 @@ describe("Router suite.", function() {
 	describe('Testing simple route',function(){
 		var href ='#/user/jhon';
 
-		beforeEach(function(done){
+		beforeEach(function(){
 			router = new Router();
 			router.run('#/');
-			done();
 		});
-		afterEach(function(done){
+		afterEach(function(){
 			if(router && router.destroy)
 				router.destroy();
 			router = null;
-			done();
 		});
 
 		describe("Once router is instantiated,",function(){
 			it('it is defined',function(){
-		  		expect(router).to.be.an(Router);
+		  		expect(router).to.be.a(Router);
 			});
-			it("it has ['redirect','setLocation','add','get','addRoute','play','pause'] methods",function(done){
-				['redirect','add','get','addRoute','play','pause','setLocation'].forEach(function(prop){
+			it("it has ['redirect','setLocation','add','get','addRoute','play','pause'] methods",function(){
+				var properties = ['redirect','add','get','addRoute','play','pause','setLocation'],
+					prop;
+				for(var i = 0, len = properties.length; i < len; i++){
+					prop = properties[i];
 					expect(router).to.have.property(prop);
-				});
-				done();
+				}
 			});		
 		});
 
 		describe("Navigating",function(){
 			it('it runs "befores"',function(done){
-				var bp = new promise();
 				router
 					.before(function(req,next){
 						expect(req).to.have.property('href');
-						bp.solve();
-						next();
-					}).add('/user/:username',function(){});
-				bp.on=done;
-				window.document.location.href = href;
+						done();
+					})
+					.add('/user/:username',function(){});
+				window.location.href = href;
 			});
 		});
 
 		describe("Navigating",function(){
 			it('it reachs /user/:username route, and :username is "jhon"',function(done){
-				var p = new promise();
 				router.add('/user/:username',function(req,next){					
-					p.solve(null,req);
-				})
-				p.on=function(err,req){
-					expect(req).to.have.property('href','#/user/jhon');
-					expect(req.params).to.have.keys('username','jhon');
-					done()
-				};
-				window.document.location.href = href;			
+					expect(req).to.have.property('href')
+					expect(req.href).to.be('#/user/jhon');
+					expect(req.params).to.have.keys('username')
+					expect(req.params.username).to.be('jhon');
+					done();
+				});
+				window.location.href = href;			
 			});
 		});
 	});
 
 	describe('Testing',function(){
 		
-		beforeEach(function(done){
+		beforeEach(function(){
 			router = new Router().run('#/');
-			done();
 		});
-		afterEach(function(done){
+		afterEach(function(){
 			if(router && router.destroy)
 				router.destroy();
 			router = null;
-			done();
 		});
 
 		describe('queries',function(){
 			describe('calling #/user?a=b',function(){
 				it('query exists in req and its property "a" has value "b" ',function(done){
-					var p = new promise();
 					router.add('#/user',function(req,next){
-						p.solve(null,req);
-					});
-					p.on=function(err,req){
 						expect(req).to.have.property('query');
-						expect(req.query).to.have.keys('a','b');
+						expect(req.query).to.have.key('a')
+						expect(req.query.a).to.be('b');
 						done();
-					};
-					window.document.location.href = '#/user?a=b&other=value';
+					});
+					window.location.href = '#/user?a=b&other=value';
 				});
 			});
 		});
@@ -114,19 +105,14 @@ describe("Router suite.", function() {
 		describe('options',function(){
 			describe('setting ignorecase to false',function(){				
 				it('ignore #/user calling #/User',function(done){
-					var p = new promise();
 					router = new Router({ignorecase:false})
 						.add('#/user',function(){
-							p.solve();
+							done('should not reach this because of ignorecase option')
 						})
 						.errors(404, function( err, href){
-							p.reject(err);
+							done();
 					    });
-					p.on=function(err){
-						expect(err).to.be.ok();
-						done();
-					};
-					window.document.location.href = '#/User';
+					window.location.href = '#/User';
 				});
 			});
 		});
@@ -147,28 +133,28 @@ describe("Router suite.", function() {
 						expect(req.get).to.be.a('function');
 						done();
 					};
-					window.document.location.href = '#/user/jhon?surname=snow';
+					window.location.href = '#/user/jhon?surname=snow';
 				});
 				it('req.get("surname") is snow',function(done){
 					p.on=function(err,req){
-						req.get('surname').should.be.equal('snow');
+						expect(req.get('surname')).to.be('snow');
 						done();
 					};
-					window.document.location.href = '#/user/jhon?surname=snow';
+					window.location.href = '#/user/jhon?surname=snow';
 				});
 				it('req.get("name") is jhon',function(done){
 					p.on=function(err,req){
-						req.get('name').should.be.equal('jhon');
+						expect(req.get('name')).to.be('jhon');
 						done();
 					};
-					window.document.location.href = '#/user/jhon?surname=snow';
+					window.location.href = '#/user/jhon?surname=snow';
 				});
 				it('req.get("address","fourth street") return the default value "fourth street"',function(done){
 					p.on=function(err,req){
-						req.get("address","fourth street").should.be.equal('fourth street');
+						expect(req.get("address","fourth street")).to.be('fourth street');
 						done();
 					};
-					window.document.location.href = '#/user/jhon?surname=snow';
+					window.location.href = '#/user/jhon?surname=snow';
 				});
 			});
 		});
@@ -194,25 +180,25 @@ describe("Router suite.", function() {
 			describe('defining #/user/* and calling #/user/jhon',function(){
 				it('it will match',function(done){
 					p.on=done;
-					window.document.location.href = '#/user/jhon';
+					window.location.href = '#/user/jhon';
 				});
 				it('but will not match #/user/jhon/snow',function(done){
 					errp.on=function(err,href){
-						href.should.be.a('string');
+						expect(href).to.be.a('string');
 						done();
 					};
-					window.document.location.href = '#/user/jhon/snow';
+					window.location.href = '#/user/jhon/snow';
 				});
 			});
 
 			describe('defining #/multi/** and calling #/multi/users',function(){
 				it('it will match',function(done){
 					multi.on=done;
-					window.document.location.href = '#/multi/users';
+					window.location.href = '#/multi/users';
 				});
 				it('and will match #/multi/users/is/a/crowd',function(done){
 					multi.on=done;
-					window.document.location.href = '#/multi/users/is/a/crowd';
+					window.location.href = '#/multi/users/is/a/crowd';
 				});
 			});
 
@@ -248,43 +234,43 @@ describe("Router suite.", function() {
 			describe('Defining a before, two matching routes and an error route',function(){
 				it('in the before next is function (and so req.hasNext)',function(done){
 					bef.on=function(err,req,next){
-						next.should.be.a('function');
-						req.hasNext.should.be.true;
+						expect(next).to.be.a('function');
+						expect(req.hasNext).to.be(true);
 						done();
 					}
-					window.document.location.href = '#/user/jhon';
+					window.location.href = '#/user/jhon';
 				});
 				it('the next callback in before is called (and so req.hasNext)',function(done){
 					first.on=function(err,req,next){
-						next.should.be.a('function');
-						req.hasNext.should.be.true;
+						expect(next).to.be.a('function');
+						expect(req.hasNext).to.be(true);
 						done();
 					};
-					window.document.location.href = '#/user/jhon';
+					window.location.href = '#/user/jhon';
 				});
 				it('the next callback in first route is called and so req.hasNext is false',function(done){
 					second.on=function(err,req,next){
 						expect(next).to.be.a('function');
-						req.hasNext.should.be.false;
+						expect(req.hasNext).to.be(false);
 						done();
 					};
-					window.document.location.href = '#/user/jhon';
+					window.location.href = '#/user/jhon';
 				});
 				it('the next callback in second route,called without an error, produce an error',function(done){
 					errp.on=function(err,href,code){
-						code.should.be.equal(500);
-						href.should.be.equal('#/user/jhon');
+						expect(code).to.be(500);
+						expect(href).to.be('#/user/jhon');
 						done();
 					};
-					window.document.location.href = '#/user/jhon';
+					window.location.href = '#/user/jhon';
 				});
 				it('the next callback in first route is called with a 500 error',function(done){
 					errp.on=function(err,href,code){
-						code.should.be.equal(500);
-						href.should.be.equal('#/user/admin');
+						expect(code).to.be(500);
+						expect(href).to.be('#/user/admin');
 						done();
 					};
-					window.document.location.href = '#/user/admin';
+					window.location.href = '#/user/admin';
 				});
 			});
 		});
@@ -319,19 +305,19 @@ describe("Router suite.", function() {
 			describe('Defining a before, two matching routes and an error route',function(){
 				it('the next callback in first route is called with a 500 error',function(done){
 					errp.on=function(err,href){
-						err.should.be.equal('error message');
-						href.should.be.equal('#/user/admin');
+						expect(err).to.be('error message');
+						expect(href).to.be('#/user/admin');
 						done();
 					};
-					window.document.location.href = '#/user/admin';
+					window.location.href = '#/user/admin';
 				});
 				it('the next callback in before is called with a 500 error',function(done){
 					errp.on=function(err,href){
-						err.should.be.equal('error message');
-						href.should.be.equal('#/user/fireerror');
+						expect(err).to.be('error message');
+						expect(href).to.be('#/user/fireerror');
 						done();
 					};
-					window.document.location.href = '#/user/fireerror';
+					window.location.href = '#/user/fireerror';
 				});
 			});
 		});
@@ -355,7 +341,7 @@ describe("Router suite.", function() {
 			describe('pause should',function(){
 				it('stop route handling',function(done){
 					first.on=done;
-					window.document.location.href = '#/path/first';
+					window.location.href = '#/path/first';
 					setTimeout(function(){
 						first.solve();
 					},1);					
@@ -366,7 +352,7 @@ describe("Router suite.", function() {
 				it('restore route handling',function(done){
 					router.play();
 					second.on=done;
-					window.document.location.href = '#/path/second';
+					window.location.href = '#/path/second';
 				});
 			});
 
@@ -374,7 +360,7 @@ describe("Router suite.", function() {
 				it('change href',function(){
 					router.play();
 					router.setLocation('#/new/location');
-					/#\/new\/location$/.test(window.document.location.href).should.be.ok;
+					expect(window.location.href).to.match(/#\/new\/location$/);
 				});
 			});
 
@@ -382,7 +368,7 @@ describe("Router suite.", function() {
 				it('change href and fire route',function(done){
 					router.play();
 					second.on=function(){
-						/#\/path\/second$/.test(window.document.location.href).should.be.ok;
+						expect(window.location.href).to.match(/#\/path\/second$/);
 						done();	
 					};
 					router.redirect('#/path/second');					
@@ -391,7 +377,7 @@ describe("Router suite.", function() {
 				it('change href and fire route even if change only query params',function(done){
 					router.play();
 					second.on=function(){
-						/#\/path\/second\?q=hi$/.test(window.document.location.href).should.be.ok;
+						expect(window.location.href).to.match(/#\/path\/second\?q=hi$/);
 						done();	
 					};
 					router.redirect('#/path/second?q=hi');					
